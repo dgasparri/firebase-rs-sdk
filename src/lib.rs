605f4340754a48a2a1f959d8efd0dbcf7511a0e9
Duccio Marco Gasparri <553197+dgasparri@users.noreply.github.com>
2025-10-27 23:55:35 +0100

    wasm: performance

--- src/lib.rs
+++ src/lib.rs
@@ -24,18 +24,11 @@ pub mod logger;
 pub mod messaging;
 
 // TODO(async-wasm): Re-enable performance module in Stage 3.
-// #[cfg(not(target_arch = "wasm32"))]
-// pub mod performance;
-// #[cfg(target_arch = "wasm32")]
-// pub mod performance {}
+//pub mod performance;
 pub mod platform;
 pub mod remote_config;
 
-// TODO(async-wasm): Re-enable storage when Stage 3 auditing completes.
-// #[cfg(not(target_arch = "wasm32"))]
 pub mod storage;
-// #[cfg(target_arch = "wasm32")]
-// pub mod storage {}
 
 pub mod util;
 
