6aaabba3ef709bfa29d2118f8753c6de14489c2a
Duccio Marco Gasparri <553197+dgasparri@users.noreply.github.com>
2025-10-29 10:40:06 +0100

    blocking: round 1

--- src/blocking/database.rs
+++ src/blocking/database.rs
@@ -1,4 +1,148 @@
-use futures::executor::block_on;
+use super::{RT, block_on, block_on_methods};
+
+//TODO
+pub use crate::database::error;
+
+
+
+pub fn get_database(app: Option<FirebaseApp>) -> DatabaseResult<Arc<Database>> {
+    block_on(crate::database::api::get_database_async(app))
+}
+
+pub fn push(reference: &DatabaseReference) -> DatabaseResult<DatabaseReference> {
+    block_on(reference.push_async())
+}
+
+pub fn push_with_value<V>(
+    reference: &DatabaseReference,
+    value: V,
+) -> DatabaseResult<DatabaseReference>
+where
+    V: Into<Value>,
+{
+    block_on(reference.push_with_value_async(value))
+}
+
+
+pub fn on_child_added<F>(
+    reference: &DatabaseReference,
+    callback: F,
+) -> DatabaseResult<ListenerRegistration>
+where
+    F: Fn(Result<ChildEvent, DatabaseError>) + Send + Sync + 'static,
+{
+    block_on(reference.on_child_added(callback))
+}
+
+pub fn on_child_changed<F>(
+    reference: &DatabaseReference,
+    callback: F,
+) -> DatabaseResult<ListenerRegistration>
+where
+    F: Fn(Result<ChildEvent, DatabaseError>) + Send + Sync + 'static,
+{
+    block_on(reference.on_child_changed(callback))
+}
+
+pub fn on_child_removed<F>(
+    reference: &DatabaseReference,
+    callback: F,
+) -> DatabaseResult<ListenerRegistration>
+where
+    F: Fn(Result<ChildEvent, DatabaseError>) + Send + Sync + 'static,
+{
+    block_on(reference.on_child_removed(callback))
+}
+
+
+pub fn run_transaction<F>(
+    reference: &DatabaseReference,
+    mut update: F,
+) -> DatabaseResult<TransactionResult>
+where
+    F: FnMut(Value) -> Option<Value>,
+{
+    block_on(reference.run_transaction(|value| update(value)))
+}
+
+pub fn set_priority<P>(reference: &DatabaseReference, priority: P) -> DatabaseResult<()>
+where
+    P: Into<Value>,
+{
+    block_on(reference.set_priority(priority))
+}
+
+
+pub fn set_with_priority<V, P>(
+    reference: &DatabaseReference,
+    value: V,
+    priority: P,
+) -> DatabaseResult<()>
+where
+    V: Into<Value>,
+    P: Into<Value>,
+{
+    block_on(reference.set_with_priority(value, priority))
+}
+
+
+
+// Database,
+// DatabaseQuery,
+// DatabaseReference,
+// Sync API re-exports
+pub use crate::database::api::{
+    end_at, end_at_with_key, end_before, end_before_with_key, equal_to, equal_to_with_key,
+     limit_to_first, limit_to_last,  order_by_child, order_by_key, order_by_priority, order_by_value, 
+     query, register_database_component,  
+     start_after, start_after_with_key, start_at, start_at_with_key, ChildEvent,
+    ChildEventType, DataSnapshot,    ListenerRegistration,
+    QueryConstraint, TransactionResult,
+};
+
+
+struct Database {
+    inner: crate::database::api::Database,
+}
+
+impl Database {
+    fn new(app: FirebaseApp) -> Self {
+        Self {
+            inner: crate::database::api::Database::new(app),
+        }
+    }
+
+    block_on_methods! {
+        fn handle_realtime_action(&self, action: &str, body: &serde_json::Value) -> DatabaseResult<()>;
+        fn handle_realtime_data(&self, action: &str, body: &serde_json::Value) -> DatabaseResult<()>;
+        fn register_listener(&self, target: ListenerTarget, kind: ListenerKind) -> DatabaseResult<ListenerRegistration>;
+        fn revoke_listener(&self, body: &serde_json::Value);
+        fn dispatch_listeners(&self, changed_path: &[String], old_root: &Value, new_root: &Value) -> DatabaseResult<()>;
+        fn root_snapshot(&self) -> DatabaseResult<Value> ;
+        fn snapshot_from_root(&self,target: &ListenerTarget,root: &Value) -> DatabaseResult<DataSnapshot>;
+        fn snapshot_for_target(&self, target: &ListenerTarget) -> DatabaseResult<DataSnapshot>;
+        fn go_online() -> DatabaseResult<()>;
+        fn go_offline() -> DatabaseResult<()>;
+    }
+}
+
+
+
+
+
+
+
+
+
+// TODO
+pub use crate::database::error::DatabaseResult;
+// TODO
+pub use crate::database::on_disconnect::OnDisconnect;
+// TODO
+pub use crate::database::server_value::{increment, server_timestamp};
+
+
+
 
 impl Database {
 
